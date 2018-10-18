96e5a72a00c94b5e95953d4fb9a24ba278a3898f
ahaskic1 <ahaskic1@etf.unsa.ba>
2018-10-18 09:40:45 +0200

    initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,15 @@
+package sample;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Unesite broj n:");
+        int broj=scanner.nextInt();
+
+    }
+}

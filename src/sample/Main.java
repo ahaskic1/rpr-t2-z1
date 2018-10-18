5c33dedbb90fb4c5f66e844dc5fa8575efe07bb6
ahaskic1 <ahaskic1@etf.unsa.ba>
2018-10-18 09:53:10 +0200

    initial commit1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,13 +3,32 @@ package sample;
 
 import java.util.Scanner;
 
+
 public class Main {
+    static int sumaCifara(int n)
+    {
+        int suma=0;
+        while(n!=0)
+        {
+            suma+=n%10;
+            n/=10;
+        }
+        return suma;
+    }
+
 
 
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Unesite broj n:");
         int broj=scanner.nextInt();
+        for(int i=1;i<broj;i++)
+        {
+            if((broj % sumaCifara(i)) == 0)
+            {
+                System.out.println(i);
+            }
+        }
 
     }
 }
